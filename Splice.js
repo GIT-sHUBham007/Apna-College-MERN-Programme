de6e5dd9c57466a4edc0beb7447497ce7cae4a34
GIT-sHUBham007 <147336124+GIT-sHUBham007@users.noreply.github.com>
2023-10-12 10:02:27 +0530

    Add files via upload

---
+++ Splice.js
@@ -0,0 +1,4 @@
+                    //  Splice
+let months = ["march", "february", "april", "may"];
+months.splice(0,2, "february", "march");  //starting with 0 index element, I want to delete 2 element from the array and want to add february and march
+console.log(months);
